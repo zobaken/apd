42fb205ea70cdc571b6d358cb42589fd8965ca00
Nikolay Neizvesny <zobaken@gmail.com>
2017-02-12 16:25:29 +0200

    fix bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ if(!defined('APD_ROOT')) {
     define('APD_ROOT', realpath(__DIR__));
     if (file_exists(APD_ROOT . '/vendor/autoload.php')) {
         require_once APD_ROOT . '/vendor/autoload.php';
-    } elseif(file_exists(APD_ROOT . '/../autoload.php')) {
-        require_once APD_ROOT . '/../autoload.php';
+    } elseif(file_exists(APD_ROOT . '/../../autoload.php')) {
+        require_once APD_ROOT . '/../../autoload.php';
     }
 }
